9ab8f48999c8995109ee7c2c58f545c61c3d0a2f
alg4ret3 <sergiom8alexanderd8@gmail.com>
2026-04-09 17:18:51 -0500

    feat: Actualización de services

--- E-commerceFront_Next/next.config.ts
+++ E-commerceFront_Next/next.config.ts
@@ -16,6 +16,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'appealable-tyson-abstractedly.ngrok-free.dev',
+        pathname: '/static/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
